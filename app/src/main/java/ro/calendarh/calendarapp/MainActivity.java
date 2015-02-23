cad1419b03c91f5074c8796a65b21f93399c6102
eight9080 <eight9080@gmail.com>
2015-02-24 00:33:55 +0200

    rename to today quote

--- app/src/main/java/ro/calendarh/calendarapp/MainActivity.java
+++ app/src/main/java/ro/calendarh/calendarapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +11,6 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Locale;
 
 
@@ -27,8 +25,8 @@ public class MainActivity extends ActionBarActivity {
         Calendar calendar = Calendar.getInstance();
         dateLabel.setText(calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US) + " " + calendar.get(Calendar.DAY_OF_MONTH) + ", " + calendar.get(Calendar.YEAR));
 
-        TextView todayActivity = (TextView)findViewById(R.id.todayActivity);
-        todayActivity.setText("“If you spend your whole life waiting for the storm, you’ll never enjoy the sunshine.” -Morris West");
+        TextView todayQuote = (TextView)findViewById(R.id.todayQuote);
+        todayQuote.setText("“If you spend your whole life waiting for the storm, you’ll never enjoy the sunshine.” -Morris West");
     }
 
 
