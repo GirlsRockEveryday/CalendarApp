404c9445c130b936cd5077ae4ee107fdc5b5bd99
eight9080 <eight9080@gmail.com>
2015-02-24 00:30:21 +0200

    Added label with date and today tip

--- app/src/main/java/ro/calendarh/calendarapp/MainActivity.java
+++ app/src/main/java/ro/calendarh/calendarapp/MainActivity.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,6 +22,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView dateLabel = (TextView)findViewById(R.id.dateLabel);
+        Calendar calendar = Calendar.getInstance();
+        dateLabel.setText(calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US) + " " + calendar.get(Calendar.DAY_OF_MONTH) + ", " + calendar.get(Calendar.YEAR));
+
+        TextView todayActivity = (TextView)findViewById(R.id.todayActivity);
+        todayActivity.setText("“If you spend your whole life waiting for the storm, you’ll never enjoy the sunshine.” -Morris West");
     }
 
 
