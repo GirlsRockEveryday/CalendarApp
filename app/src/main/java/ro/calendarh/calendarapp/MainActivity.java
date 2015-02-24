2262561cf2d43643e3d2f480c8cc73500e43e0ec
eight9080 <eight9080@gmail.com>
2015-02-24 22:51:38 +0200

    added welcome dialog

--- app/src/main/java/ro/calendarh/calendarapp/MainActivity.java
+++ app/src/main/java/ro/calendarh/calendarapp/MainActivity.java
@@ -1,14 +1,20 @@
 package ro.calendarh.calendarapp;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Locale;
@@ -16,10 +22,18 @@ import java.util.Locale;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
+
+//    private ShareActionProvider mShareActionProvider;
+    private SharedPreferences mSharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayWelcome();
 
         TextView dateLabel = (TextView)findViewById(R.id.dateLabel);
         Calendar calendar = Calendar.getInstance();
@@ -54,5 +68,41 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public View onCreateView(String name, @NonNull Context context, @NonNull AttributeSet attrs) {
         return super.onCreateView(name, context, attrs);
+    }
+
+    private void displayWelcome() {
+        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+        String name = mSharedPreferences.getString(PREF_NAME, "");
+        if (name.length() > 0) {
+            Toast.makeText(this, "Welcome back, " + name + "!", Toast.LENGTH_LONG).show();
+        } else {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Hello!");
+            alert.setMessage("What is your name?");
+
+            final EditText input = new EditText(this);
+            alert.setView(input);
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String inputName = input.getText().toString();
+                    SharedPreferences.Editor edit = mSharedPreferences.edit();
+                    edit.putString(PREF_NAME, inputName);
+                    edit.commit();
+
+                    Toast.makeText(getApplicationContext(), "Welcome, " + inputName + "!", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            alert.show();
+        }
     }
 }
