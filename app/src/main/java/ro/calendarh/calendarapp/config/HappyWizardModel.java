83f8432b3b50f45d3f17c62edf96a6d237a80792
ralukame <raluca.stanculescu@gmail.com>
2015-09-16 19:08:24 +0300

    integrated the wizard pager-runtime class loading for  com.example.android.wizardpager.wizard.ui.StepPagerStrip  gradle issue

---
+++ app/src/main/java/ro/calendarh/calendarapp/config/HappyWizardModel.java
@@ -0,0 +1,25 @@
+package ro.calendarh.calendarapp.config;
+
+import android.content.Context;
+import com.tech.freak.wizardpager.model.AbstractWizardModel;
+import com.tech.freak.wizardpager.model.PageList;
+
+import ro.calendarh.calendarapp.config.wizard.model.CustomerInfoPage;
+import ro.calendarh.calendarapp.config.wizard.model.SalutationPage;
+
+/**
+ * 2 pages, salutation page and info page
+ */
+public class HappyWizardModel extends AbstractWizardModel {
+
+    public HappyWizardModel(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected PageList onNewRootPageList() {
+        return new PageList(new SalutationPage(this,"Salutations!"),
+                            new CustomerInfoPage(this, "Your info").setRequired(true)
+        );
+    }
+}
