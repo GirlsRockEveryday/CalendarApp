b85ca4b2c5e08a273fc7747c35a0efb8a5962ddc
ralukame <raluca.stanculescu@gmail.com>
2015-10-06 13:12:49 +0300

    date check mecanism

--- app/src/main/java/ro/calendarh/calendarapp/notification/content/ActivityChooser.java
+++ app/src/main/java/ro/calendarh/calendarapp/notification/content/ActivityChooser.java
@@ -4,7 +4,6 @@ import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import static ro.calendarh.calendarapp.config.wizard.PreferenceHelper.LATEST_DAY;
@@ -22,22 +21,28 @@ public class ActivityChooser {
     }
 
     public ActivityBean getActivity() {
+        doDateCheck();
+
+        JsonParserAdaptor parser= new JsonParserAdaptor(assetManager);
+        JsonArray elements=parser.parseActivities();
+        ActivityBean activity = new Gson().fromJson(elements.get(getLatestDay()), ActivityBean.class);
+        return activity;
+    }
+
+    private void doDateCheck() {
         SharedPreferences.Editor edit = mSharedPreferences.edit();
 
         Calendar previousDateCal=removeTimeFromDate(getPreviousDate());
         Calendar currentDate=removeTimeFromDate(new Date());
 
         if(previousDateCal.compareTo(currentDate)==-1) {  //we have a new day, show a different
-            edit.putInt(LATEST_DAY,  mSharedPreferences.getInt(LATEST_DAY,0)+1);
+            edit.putInt(LATEST_DAY,  getLatestDay() +1);
             edit.putLong(LATEST_DATE_OPENED, new Date().getTime());
         }
+    }
 
-        int latestDay=mSharedPreferences.getInt(LATEST_DAY,0);
-        JsonParserAdaptor parser= new JsonParserAdaptor(assetManager);
-        JsonArray elements=parser.parseActivities();
-        Gson myGson = new Gson();
-        ActivityBean activity = myGson.fromJson(elements.get(latestDay), ActivityBean.class);
-        return activity;
+    private int getLatestDay() {
+        return mSharedPreferences.getInt(LATEST_DAY,0);
     }
 
     private Calendar removeTimeFromDate(Date date) {
